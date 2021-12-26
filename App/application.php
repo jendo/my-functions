9663fcfd4f67b074ce41d66c632e8dfc7ec6f0fe
Lukas Fruno <fruno.lu@gmail.com>
2021-12-26 10:05:47 +0100

    Added functions

--- App/application.php
+++ App/application.php
@@ -1,7 +1,18 @@
 <?php
-//W3resource Exercise 13
-//Write a PHP script to display string, values within a table.
-//uloha 4.2, vykresli pomocou foreach
+
+namespace App;
+require_once 'functions/functions.php';
+
+// Exercise 4.1
+
+$myList = [1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 9];
+$result = removeDuplicatesList($myList);
+var_dump($result);
+
+
+// Exercise 4.2, vykresli pomocou foreach
+// W3resource Exercise 13
+// Write a PHP script to display string, values within a table.
 
 
 $tableRows = [];
@@ -9,14 +20,32 @@ $tableRows['A'] = 1000;
 $tableRows['B'] = 1200;
 $tableRows['C'] = 1400;
 
-echo '<table border=1 cellspacing=0 cellpading=0>';
+echo '<table border=1 cellspacing=0 cellpadding=0>';
 foreach ($tableRows as $employee => $tableRow) {
     echo "Salary of Mr. $employee is $tableRow <br>";
 }
 echo '</table>';
 
+//Exercise 4.3.1
+// W3resource Exercise 39
+// Look at the exercise 4.3.2 and 4.3.3
 
 
-$myList = [1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 9];
-$result = removeDuplicatesList($myList);
-var_dump($result);
+//Exercise 4.3.2
+
+$file = 'application.php';
+echo fileExistAndSize($file);
+
+
+//Exercise 4.3.3
+writingToAFile('test.txt','ahoj');
+
+
+//Exercise 4.4 function randomWords
+$words = [];
+$words [] = "PHP";
+$words [] = "AJ";
+$words [] = "sport";
+
+randomWords($words);
+
--- App/functions/functions.php
+++ App/functions/functions.php
@@ -1,6 +1,8 @@
 <?php
 
 /**
+ * Exercise 4.1
+ *
  * W3r Basic Exercise 35
  *
  * @param array $list1
@@ -13,11 +15,14 @@ function removeDuplicatesList(array $list1): array
 
 
 /**
+ * 4.3.2
+ *
  * @param string $file
- * @return string
+ * @return void
+ * i
+ * ng
  */
-$file = 'FunctionFileExistAndSize.php';
-function FileExistAndSize(string $file): string
+function fileExistAndSize(string $file): void
 {
 
     if (file_exists($file)) {
@@ -25,4 +30,52 @@ function FileExistAndSize(string $file): string
     } else {
         echo 'This file doesnt exist.';
     }
+}
+
+/**
+ * Exercise 4.3.3
+ *
+ * @param string $filename
+ * @param string $text
+ * @return void
+ */
+function writingToAFile(string $filename, string $text): void
+{
+    if (file_exists($filename) === false) {
+        echo sprintf('File %s exist', $filename);
+        return;
+    }
+    $myFile = fopen($filename, 'wb+');
+    if ($myFile === false) {
+        echo sprintf('File %s was not open.', $filename);
+        return;
+    }
+    if (fwrite($myFile, $text)=== false){
+        echo sprintf('Text %s was not wrote to the file %s.', $text, $filename);
+        return;
+    }
+    if (fclose($myFile) === false) {
+        echo sprintf('File %s can not be closed', $filename);
+        return;
+    }
+    echo sprintf('Text %s was written to the file %s',$text, $filename);
+}
+
+/**
+ * Exercise 4.4
+ *
+ * @param array $words
+ * @return void
+ */
+function randomWords(array $words):void
+{
+    $rand_keys = array_rand($words);
+    do {
+        if (isset($words[$rand_keys])) {
+            echo($words[$rand_keys] . "\n");
+            unset($words[$rand_keys]);
+        } elseif (empty($words($rand_keys))) {
+            echo 'This word have been chosen already.';
+        }
+    } while ($words && count(array_filter($words, 'is_null')) !== count($words));
 }
